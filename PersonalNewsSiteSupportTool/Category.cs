da399fa8531026a22f349ccd99b6869d78b1596d
denimu <42787839+kurodenimu@users.noreply.github.com>
2019-12-28 06:53:48 +0900

    Initial Commit

---
+++ PersonalNewsSiteSupportTool/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalNewsSiteSupportTool
+{
+    public class Category
+    {
+        public String Name { get; set; }
+        public String Id { get; set; }
+
+
+    }
+}
