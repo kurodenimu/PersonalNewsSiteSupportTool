7865903296f037a231c00768691807fc0637b08e
denimu <denimupaueru@gmail.com>
2019-12-28 06:53:48 +0900

    Initial Commit

---
+++ PersonalNewsSiteSupportTool/ClipBoardWatcher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace PersonalNewsSiteSupportTool
+{
+    public class ClipboardWatcher
+    {
+        [DllImport("user32.dll")]
+        private static extern bool AddClipboardFormatListener(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern bool RemoveClipboardFormatListener(IntPtr hWnd);
+
+
+        private const int WM_DRAWCLIPBOARD = 0x031D;
+
+
+        IntPtr handle;
+        HwndSource hwndSource = null;
+
+
+        public event EventHandler DrawClipboard;
+        //イベント起動
+        private void raiseDrawClipboard()
+        {
+            DrawClipboard?.Invoke(this, EventArgs.Empty);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_DRAWCLIPBOARD)
+            {
+                this.raiseDrawClipboard();
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="handle">System.Windows.Interop.WindowInteropHelper(this).Handleとかで取得</param>
+        public ClipboardWatcher(IntPtr handle)
+        {
+            hwndSource = HwndSource.FromHwnd(handle);
+            hwndSource.AddHook(WndProc);
+            this.handle = handle;
+            //AddClipboardFormatListener(handle);
+        }
+        //クリップボード監視開始
+        public void Start()
+        {
+            AddClipboardFormatListener(handle);
+        }
+        //クリップボード監視停止
+        public void Stop()
+        {
+            RemoveClipboardFormatListener(handle);
+        }
+    }
+}
---
+++ PersonalNewsSiteSupportTool/NotifyIconWrapper.cs
@@ -0,0 +1,45 @@
+﻿namespace PersonalNewsSiteSupportTool
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Forms;
+    using Application = System.Windows.Application;
+
+    public partial class NotifyIconWrapper : Control
+    {
+
+        /// <summary>
+        /// NotifyIconWrapper クラス を生成、初期化します。
+        /// </summary>
+        public NotifyIconWrapper()
+        {
+            // コンポーネントの初期化
+            this.InitializeComponent();
+
+            // コンテキストメニューのイベントを設定
+            this.toolStripMenuItem_Exit.Click += this.toolStripMenuItem_Exit_Click;
+        }
+
+        /// <summary>
+        /// コンテナ を指定して NotifyIconWrapper クラス を生成、初期化します。
+        /// </summary>
+        /// <param name="container">コンテナ</param>
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "終了" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void toolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            // 現在のアプリケーションを終了
+            Application.Current.Shutdown();
+        }
+    }
+}
