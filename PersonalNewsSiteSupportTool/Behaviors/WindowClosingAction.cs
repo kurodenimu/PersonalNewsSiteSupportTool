460c7692324f531347d31f043abf4cb1b93e6888
denimu <42787839+kurodenimu@users.noreply.github.com>
2020-01-05 00:59:58 +0900

    極力MVVMに沿うようにリファクタリング

---
+++ PersonalNewsSiteSupportTool/Behaviors/WindowClosingAction.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace PersonalNewsSiteSupportTool.Behaviors
+{
+    class WindowClosingAction : TriggerAction<Window>
+    {
+        protected override void Invoke(object parameter)
+        {
+            var cancelEventArgs = parameter as CancelEventArgs;
+            cancelEventArgs.Cancel = true;
+            var window = Window.GetWindow(AssociatedObject);
+            window.Hide();
+        }
+    }
+}
---
+++ PersonalNewsSiteSupportTool/Views/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PersonalNewsSiteSupportTool.ViewModels;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace PersonalNewsSiteSupportTool.Views
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow instance = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            instance = this;
+            this.DataContext = new MainWindowModel();
+        }
+    }
+}
