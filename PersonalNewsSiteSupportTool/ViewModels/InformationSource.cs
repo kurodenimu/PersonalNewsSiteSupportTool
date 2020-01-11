42779dd0d499a3d0073601dba16977ee70cb2bbb
denimu <42787839+kurodenimu@users.noreply.github.com>
2020-01-11 21:46:40 +0900

    #6 Viaの入力欄を追加した その他、軽微な修正 ・不要なメンバ変数削除 ・不要なプロパティ変更通知を削除

---
+++ PersonalNewsSiteSupportTool/ViewModels/InformationSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalNewsSiteSupportTool.ViewModels
+{
+    class InformationSource
+    {
+        public String Name { get; set; }
+        public String Data { get; set; }
+    }
+}
