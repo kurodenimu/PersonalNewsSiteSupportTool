3f22e976761395f90cec5aad82dc51093e6ec01f
denimu <42787839+kurodenimu@users.noreply.github.com>
2020-02-08 02:20:21 +0900

    ライセンスフォルダの名前を変更

--- PersonalNewsSiteSupportTool/Properties/AssemblyInfo.cs
+++ PersonalNewsSiteSupportTool/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("ja-JP")]
+[assembly: NeutralResourcesLanguage("")]
