61f1d3947d2030e5e5499dcc8dccbbaa67121ad5
denimu <denimupaueru@gmail.com>
2021-07-16 19:22:57 +0900

    #1 各種設定の定義ファイル化。その他、設定・パッケージの整理等。

---
+++ PersonalNewsSiteSupportTool/Models/ConfigManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace PersonalNewsSiteSupportTool.Models
+{
+    public sealed class ConfigManager
+    {
+
+        public static Config config { get; private set; }
+
+        private ConfigManager()
+        {
+            
+        }
+
+        public static void ReloadConfig()
+        {
+            string json = File.ReadAllText(@".\config.json", System.Text.Encoding.UTF8);
+            var options = new JsonSerializerOptions
+            {
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All),
+                WriteIndented = true
+            };
+            config = JsonSerializer.Deserialize<Config>(json, options);
+        }
+    }
+    public class Config
+    {
+        public string WatchWord { get; set; } = "";
+        public string SavePath { get; set; } = "";
+        public string OutFilePrefix { get; set; } = "";
+        public string OutFileSuffix { get; set; } = "";
+        public string NewLine { get; set; } = "\r\n";
+        public string CategoryPrefix { get; set; } = "";
+        public string CategorySuffix { get; set; } = "";
+        public string ViaPrefix { get; set; } = "";
+        public string ViaSuffix { get; set; } = "";
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:コレクション プロパティは読み取り専用でなければなりません", Justification = "<保留中>")]
+        public List<KeyValuePair<String, String>> Categories { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:コレクション プロパティは読み取り専用でなければなりません", Justification = "<保留中>")]
+        public List<KeyValuePair<String, String>> InformationSources { get; set; }
+    }
+}
