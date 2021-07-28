ef4d75d9072b989dd20290f671dbb1b967ca242e
denimu <denimupaueru@gmail.com>
2021-07-28 20:15:59 +0900

    メッセージボックスを出す際のウィンドウ制御 #42

--- PersonalNewsSiteSupportTool/ViewModels/ViewModelBase.cs
+++ PersonalNewsSiteSupportTool/ViewModels/ViewModelBase.cs
@@ -18,21 +18,40 @@ namespace PersonalNewsSiteSupportTool.ViewModels
 
         public void ShowInfoMessage(string message, string title = "情報")
         {
-            Application.Current.MainWindow.Activate();
+            ActivateWindow();
             Messenger.Raise(new InformationMessage(message, title, MessageBoxImage.Information, "Information"));
         }
         public void ShowErrorMessage(string message, string title = "エラー")
         {
-            Application.Current.MainWindow.Activate();
+            ActivateWindow();
             Messenger.Raise(new InformationMessage(message, title, MessageBoxImage.Error, "Error"));
         }
         public bool ShowConfirmMessage(string message, string title = "確認")
         {
-            Application.Current.MainWindow.Activate();
+            ActivateWindow();
             var confirmationMessage = new ConfirmationMessage(message, title, MessageBoxImage.Question, MessageBoxButton.OKCancel, "Confirm");
             Messenger.Raise(confirmationMessage);
 
             return confirmationMessage.Response ?? false;
+        }
+
+        private void ActivateWindow()
+        {
+            // アクティブなウィンドウがない場合のみ、MainWindowをアクティブにする。
+            // メッセージボックスを表示するのにアクティブなウィンドウが必要なため。
+            bool existActiveWindow = false;
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.IsActive)
+                {
+                    existActiveWindow = true;
+                    break;
+                }
+            }
+            if (!existActiveWindow)
+            {
+                Application.Current.MainWindow.Activate();
+            }
         }
     }
 }
