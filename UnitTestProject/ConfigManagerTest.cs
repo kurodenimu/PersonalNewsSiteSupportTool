426601a0dfaed80f5eabe31e6905a9e0dcaa320f
denimu <42787839+kurodenimu@users.noreply.github.com>
2021-08-22 17:56:16 +0900

    ConfigManagerのテストクラス追加

---
+++ UnitTestProject/ConfigManagerTest.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonalNewsSiteSupportTool.Models;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ConfigManagerTest :TestBase
+    {
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            ClassInit();
+        }
+
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            ClassClean();
+        }
+
+        [TestMethod]
+        public void TestReloadConfig_NoFile()
+        {
+            var configManager = new PrivateType(typeof(ConfigManager));
+            try
+            {
+                // テスト用に設定ファイルのパスを書換え
+                string noFilePath = "noconfig.json";
+                configManager.SetStaticFieldOrProperty("configPath", noFilePath);
+                if (File.Exists(noFilePath))
+                {
+                    // 前のテストの不備などでファイルが残っていた場合を考慮して削除。
+                    File.Delete(noFilePath);
+                }
+
+                ConfigManager.ReloadConfig();
+
+                Assert.IsTrue(File.Exists(noFilePath), "ファイルがコピーできていません。");
+                // 設定ファイルの読込結果確認。代表して改行コードのみ確認。
+                Assert.AreEqual("\n", ConfigManager.Config.NewLine, "読み込んだ設定ファイルの改行コードの値が誤っています。");
+                File.Delete(noFilePath);
+            }
+            finally
+            {
+                // 他のテストのために設定を戻す。
+                configManager.SetStaticFieldOrProperty("configPath", TEST_CONFIG_PATH);
+            }
+        }
+
+        [TestMethod]
+        public void TestReloadConfig_ExistFile()
+        {
+            var configManager = new PrivateType(typeof(ConfigManager));
+            try
+            {
+                // テスト用に設定ファイルのパスを書換え
+                string loadFilePath = @"..\..\TestData\loadtest.json";
+                configManager.SetStaticFieldOrProperty("configPath", loadFilePath);
+
+                ConfigManager.ReloadConfig();
+
+                // 設定ファイルの読込結果確認。
+                var expConfig = new Config
+                {
+                      WatchWord = "1",
+                      IsRemoveWatchWord = false,
+                      SavePath = "2",
+                      OutFilePrefix = "3",
+                      OutFileSuffix = "4",
+                      NewLine = "5",
+                      CategoryPrefix = "6",
+                      CategorySuffix = "7",
+                      ViaPrefix = "8",
+                      ViaSuffix = "9",
+                      MergeFileName = "10",
+                      Categories = new List<KeyValuePair<string, string>>
+                      {
+                          new KeyValuePair<string, string>("Category1", "カテゴリ１")
+                      },
+                      ViaList = new List<KeyValuePair<string, string>>
+                      {
+                          new KeyValuePair<string, string>("Via1", "情報元１")
+                      }
+                };
+                AreEquals(expConfig, ConfigManager.Config);
+            }
+            finally
+            {
+                // 他のテストのために設定を戻す。
+                configManager.SetStaticFieldOrProperty("configPath", TEST_CONFIG_PATH);
+            }
+        }
+
+        [TestMethod]
+        public void TestSaveConfig()
+        {
+            var configManager = new PrivateType(typeof(ConfigManager));
+            try
+            {
+                // テスト用に設定ファイルのパスを書換え（読込）
+                string loadFilePath = @"..\..\TestData\loadtest.json";
+                configManager.SetStaticFieldOrProperty("configPath", loadFilePath);
+                ConfigManager.ReloadConfig();
+                Console.WriteLine("reloaded");
+                // テスト用に設定ファイルのパスを書換え（保存先）
+                string saveFilePath = @"..\..\ResultData\save.json";
+                configManager.SetStaticFieldOrProperty("configPath", saveFilePath);
+                ConfigManager.SaveConfig();
+                Console.WriteLine("saved");
+
+                AssertFile(loadFilePath, saveFilePath);
+            }
+            finally
+            {
+                // 他のテストのために設定を戻す。
+                configManager.SetStaticFieldOrProperty("configPath", TEST_CONFIG_PATH);
+            }
+        }
+
+
+    }
+}
---
+++ UnitTestProject/UnitTest2.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+using NLog.Targets;
+using PersonalNewsSiteSupportTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// UnitTest2 の概要の説明
+    /// </summary>
+    [TestClass]
+    public class UnitTest2 : TestBase
+    {
+        public UnitTest2()
+        {
+            //
+            // TODO: コンストラクター ロジックをここに追加します
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        //
+        // テストを作成する際には、次の追加属性を使用できます:
+        //
+        // クラス内で最初のテストを実行する前に、ClassInitialize を使用してコードを実行してください
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            ClassInit();
+        }
+        //
+        // クラス内のテストをすべて実行したら、ClassCleanup を使用してコードを実行してください
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            ClassClean();
+        }
+        //
+        // 各テストを実行する前に、TestInitialize を使用してコードを実行してください
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 各テストを実行した後に、TestCleanup を使用してコードを実行してください
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Console.WriteLine(Environment.CurrentDirectory);
+            // Console.WriteLine(PathManager.GetAppDataPath());
+            List<string> exp = new List<string>
+            {
+                "test1"
+            };
+            var act = new List<string>
+            {
+                "test2"
+            };
+
+            AreEquals(exp, act);
+        }
+    }
+}
